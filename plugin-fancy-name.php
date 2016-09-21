fb48e1519b3f0b34cba02c1c595798e507ca9cb8
Thiago Andrade <thiagoh@gmail.com>
2016-09-20 23:10:56 -0400

    :sparkles: the_content filter

--- plugin-fancy-name.php
+++ plugin-fancy-name.php
@@ -4,6 +4,19 @@ function my_first_plugin_filter_title($title) {
 	return 'The ' . $title . ' was filtered';
 }
 
-add_filter('the_title', ('my_first_plugin_filter_title'));
+function my_first_plugin_filter_content($content) {
+
+	$filteredContent = '';
+	$lines = preg_split("(\n|<[bB][rR]>)", $content);
+
+	for ($i = 0; $i < count($lines); $i++) {
+		$filteredContent .= $lines[$i] . "[end of line]<BR />\n";
+	}
+
+	return $filteredContent;
+}
+
+add_filter('the_title', 'my_first_plugin_filter_title');
+add_filter('the_content', 'my_first_plugin_filter_content');
 
 ?>
